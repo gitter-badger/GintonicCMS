dc23430c6b6d43117f288fd6d064071fdc93b4e1
Phillaf <phil.laf@gmail.com>
2015-10-07 02:45:37 -0400

    Plans no longer relevant

--- config/routes.php
+++ config/routes.php
@@ -7,12 +7,8 @@ Router::scope('/', ['plugin' => 'GintonicCMS'], function ($routes) {
     $routes->fallbacks('DashedRoute');
 
     $routes->prefix('admin', function ($routes) {
-
         $routes->connect('/users', ['controller' => 'Users']);
         $routes->connect('/users/:action/*', ['controller' => 'Users']);
-        $routes->connect('/plans', ['controller' => 'Plans']);
-        $routes->connect('/plans/:action/*', ['controller' => 'Plans']);
-
         $routes->fallbacks('DashedRoute');
     });
 
