4c22622a95de61b05efcb65e2669a16c85bacf18
Philippe Lafrance <phil.laf@gmail.com>
2015-11-26 05:27:01 -0500

    The users role is now accessible on the index and edit actions

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -4,6 +4,7 @@ namespace GintonicCMS\Controller\Admin;
 use App\Controller\Admin\AppController;
 use Cake\Event\Event;
 use Cake\ORM\TableRegistry;
+use Permissions\Model\Entity\Role;
 
 /**
  * Users Controller
@@ -18,7 +19,35 @@ class UsersController extends AppController
      */
     public function beforeFilter(Event $event)
     {
+        $this->Users->hasOne('Roles', [
+            'className' => 'Permissions.Roles',
+        ]);
+
         $action = $this->Crud->action();
         $action->config('scaffold.fields_blacklist', ['password', 'token']);
+        $action->config('scaffold.fields', [
+            'id',
+            'email',
+            'username',
+            'verified',
+            'deleted',
+            'role.role' => [
+                'formatter' => function ($name, $value, $entity) {
+                    if (isset($entity->role)) {
+                        return $entity->role->names($entity->role->role);
+                    }
+                    return 'User';
+                },
+                'options' => Role::names(),
+            ],
+        ]);
+        $this->Crud->listener('relatedModels')->relatedModels(['Roles'], 'index');
+    }
+
+    public function index()
+    {
+        $action = $this->Crud->action();
+        $action->config('scaffold.fields_blacklist', ['created', 'modified']);
+        return $this->Crud->execute();
     }
 }
